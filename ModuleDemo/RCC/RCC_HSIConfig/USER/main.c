8e40b05f50336187ae2e859fe85a29f879234c71
yich_liao <yich_liao@mail.davicom.com.tw>
2026-05-12 18:19:24 +0800

    feat(RCC): update RCC configuration for MH2203CBT6 - Change target device to MH2203CBT6 - Update USART configuration to use USART2 - Adjust memory settings for new device - Modify clock settings for improved stability
    This enhances compatibility with the MH2203CBT6 and optimizes performance.

--- ModuleDemo/RCC/RCC_HSIConfig/USER/main.c
+++ ModuleDemo/RCC/RCC_HSIConfig/USER/main.c
@@ -6,29 +6,29 @@
 
 #define PRINTF_LOG 	printf
 
-USART_TypeDef* USART_TEST = USART1;
+USART_TypeDef* USART_TEST = USART2;
 
-void UART_Configuration(uint32_t bound);
+void UART_Configuration(uint32_t BaudRate);
 void RCC_ClkConfiguration(void);
 
-	
+
 int main(void)
-{	
+{
 	RCC_ClocksTypeDef clocks;
-	
+
 	RCC_ClkConfiguration();
 	Delay_Init();
 	UART_Configuration(115200);
-	
+
 	RCC_GetClocksFreq(&clocks);
-	
+
 	PRINTF_LOG("\n");
 	PRINTF_LOG("SYSCLK: %3.1fMhz, HCLK: %3.1fMhz, PCLK1: %3.1fMhz, PCLK2: %3.1fMhz\n", \
 	(float)clocks.SYSCLK_Frequency/1000000, (float)clocks.HCLK_Frequency/1000000, \
 	(float)clocks.PCLK1_Frequency/1000000, (float)clocks.PCLK2_Frequency / 1000000);
-	
+
 	PRINTF_LOG("RCC HSI Config Test.\n");
-	
+
 	while(1)
 	{
 
@@ -41,44 +41,44 @@ void RCC_ClkConfiguration(void)
 
 	RCC_HSICmd(ENABLE);
 	while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
-	
+
 	RCC_PLLCmd(DISABLE);
-	
+
 	FLASH_SetLatency(FLASH_Latency_2);
-	
+
 	RCC_PLLConfig(RCC_PLLSource_HSI_Div1,RCC_PLLMul_27);
-	
+
 	RCC_PLLCmd(ENABLE);
 	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-	
+
 	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-	
+
 	RCC_HCLKConfig(RCC_SYSCLK_Div1);
 	RCC_PCLK1Config(RCC_HCLK_Div2);
 	RCC_PCLK2Config(RCC_HCLK_Div1);
-	
+
 	RCC_LSICmd(ENABLE);
 	while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
 }
 
-void UART_Configuration(uint32_t bound)
+void UART_Configuration(uint32_t BaudRate)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; 
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	USART_InitStructure.USART_BaudRate = bound;
+
+	USART_InitStructure.USART_BaudRate = BaudRate;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
@@ -108,4 +108,3 @@ int fputc(int c, FILE *f)
 	}
 	return (SER_PutChar(c));
 }
-
